51a32ee1dc81cbca26025c35a4d046eaeadc8279
J-912-PC <identifier.j912@gmail.com>
2021-09-28 15:40:13 +0200

    up

--- guess-the-number-console/main.js
+++ guess-the-number-console/main.js
@@ -1,1 +1,17 @@
 console.log('Hello user!')
+console.log('Try to guess the mystery number.')
+console.log('Use "submit(x)" to submit a number.')
+
+const mysteryNumber = 45
+
+const submit = (x) => {
+    if (x < mysteryNumber){
+        console.log('Too small.')
+    }
+    if (x > mysteryNumber){
+        console.log('Too big.')
+    }
+    if (x == mysteryNumber){
+        console.log('Hurrah!')
+    }
+}
