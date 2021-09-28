cfa7cb2ba03bbdb38c828612c1667d5fba0a2255
J-912-PC <identifier.j912@gmail.com>
2021-09-28 16:49:51 +0200

    up

--- guess-the-number-console/main.js
+++ guess-the-number-console/main.js
@@ -2,16 +2,23 @@ console.log('Hello user!')
 console.log('Try to guess the mystery number.')
 console.log('Use "submit(x)" to submit a number.')
 
-const mysteryNumber = 45
+const mysteryNumber = Math.round(Math.random() * 100)
 
 const submit = (x) => {
     if (x < mysteryNumber){
         console.log('Too small.')
+        document.body.style.backgroundColor = '#ff2828'
     }
     if (x > mysteryNumber){
         console.log('Too big.')
+        document.body.style.backgroundColor = '#fd9300'
     }
     if (x == mysteryNumber){
         console.log('Hurrah!')
+        document.body.style.backgroundColor = '#33ff6e'
     }
+}
+
+const cheat = () => {
+    console.log(`The mystery number is ${mysteryNumber}`)
 }
