24429b20d68993fea25139c0a26e3d0c1ec487a6
J-912-PC <identifier.j912@gmail.com>
2021-09-28 16:56:53 +0200

    up

--- guess-the-number-console/main.js
+++ guess-the-number-console/main.js
@@ -7,14 +7,17 @@ const mysteryNumber = Math.round(Math.random() * 100)
 const submit = (x) => {
     if (x < mysteryNumber){
         console.log('Too small.')
+        document.body.innerHTML = '<h1>Too small</h1>'
         document.body.style.backgroundColor = '#ff2828'
     }
     if (x > mysteryNumber){
         console.log('Too big.')
+        document.body.innerHTML = '<h1>Too big</h1>'
         document.body.style.backgroundColor = '#fd9300'
     }
     if (x == mysteryNumber){
         console.log('Hurrah!')
+        document.body.innerHTML = '<h1>Hurrah!</h1>'
         document.body.style.backgroundColor = '#33ff6e'
     }
 }
