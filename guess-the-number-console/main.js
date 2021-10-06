dc716be36e9d4a123aab36740f87e036a38d2876
J-912-PC <identifier.j912@gmail.com>
2021-10-06 11:29:20 +0200

    up

--- guess-the-number-console/main.js
+++ guess-the-number-console/main.js
@@ -5,19 +5,28 @@ console.log('Use "submit(x)" to submit a number.')
 const mysteryNumber = Math.round(Math.random() * 100)
 
 const submit = (x) => {
-    if (x < mysteryNumber){
+
+    x = parseFloat(x)
+
+    if(Number.isNaN(x)){
+        console.log('Wrong input, please give a number.')
+        document.body.innerHTML = '<h1 class="bouce">⚠️</h1>'
+        document.body.style.backgroundColor = 'red'
+    } else if (x < 0 || x > 100){
+        console.log('Number should be between 0 & 100')
+        document.body.innerHTML = '<h1 class="bouce">Number should be between 0 & 100</h1>'
+        document.body.style.backgroundColor = 'red'
+    } else if (x < mysteryNumber){
         console.log('Too small.')
-        document.body.innerHTML = '<h1>Too small</h1>'
+        document.body.innerHTML = '<h1 class="bouce">Too small</h1>'
         document.body.style.backgroundColor = '#ff2828'
-    }
-    if (x > mysteryNumber){
+    } else if (x > mysteryNumber){
         console.log('Too big.')
-        document.body.innerHTML = '<h1>Too big</h1>'
+        document.body.innerHTML = '<h1 class="bouce">Too big</h1>'
         document.body.style.backgroundColor = '#fd9300'
-    }
-    if (x == mysteryNumber){
+    } else if (x == mysteryNumber){
         console.log('Hurrah!')
-        document.body.innerHTML = '<h1>Hurrah</h1>'
+        document.body.innerHTML = '<h1 class="bouce">Hurrah</h1>'
         document.body.style.backgroundColor = '#33ff6e'
     }
 }
